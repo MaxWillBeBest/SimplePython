9aece4dd42d833aafeadc4760e8c6b22aaf19cbb
freedefine <15883197128@163.com>
2018-05-10 20:14:00 +0800

    list的函数

--- p1.py
+++ p1.py
@@ -1,8 +1,20 @@
+
 print('I will learn python well')
 print('100+200=', 100+200)
 
-print(ord('A'),
-chr(66))
+print(ord('A'), chr(66))
 
 classmates = ['Michael', 'Bob', 'Tracy']
 print(classmates[0])
+
+classmates.append('Adam')
+print(classmates[-1])
+
+classmates.insert(1,'Jack')
+print(classmates[1])
+
+classmates.pop(1)
+print(classmates[1])
+
+s = ['python', 'java', ['asp', 'php'], 'scheme']
+print(len(s))
